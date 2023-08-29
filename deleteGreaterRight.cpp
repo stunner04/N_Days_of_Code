e5b5d831dd07412f73affba95269839bf7a995bd
stunner04 <yashv0347@gmail.com>
2023-08-29 21:08:58 +0530

    Initial Commit

---
+++ deleteGreaterRight.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+class Solution
+{
+public:
+    Node *compute(Node *head)
+    {
+        Node *curr = head;
+
+        while (curr->next != NULL)
+        {
+            if (curr->data < curr->next->data)
+            {
+                curr->data = curr->next->data;
+                Node *temp = curr->next;
+                curr->next = temp->next;
+                curr = head; // tricky point to notice
+                delete temp;
+            }
+            else
+            {
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int K;
+        cin >> K;
+        struct Node *head = NULL;
+        struct Node *temp = head;
+
+        for (int i = 0; i < K; i++)
+        {
+            int data;
+            cin >> data;
+            if (head == NULL)
+                head = temp = new Node(data);
+            else
+            {
+                temp->next = new Node(data);
+                temp = temp->next;
+            }
+        }
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout << endl;
+    }
+}
